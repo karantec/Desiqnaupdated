f785e08cd2db911149e9c4dcb7a3c4a845da08ad
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-11-05 10:36:04 +0530

    Update HeroSection.jsx

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -139,7 +139,7 @@ function HeroSection() {
                     <FaArrowAltCircleRight className="text-orange-500 mt-1 mr-2 flex-shrink-0" />
                    Price before November 8 {"    "}
                                       
-                    <span className="text-red-500 text-2xl ml-2">₹4000</span>
+                    <span className="text-red-500 text-2xl ml-2">₹3280</span>
                   </div>
                 </li>
                
