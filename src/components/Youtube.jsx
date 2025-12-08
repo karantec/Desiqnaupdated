78ac25ce22fb3c17f5d15d75cce277bad660aad2
karantec <karanrana3095@gmail.com>
2025-12-09 01:45:50 +0530

    commit

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-const VIDEO_ID = "Qx6n3mgY4tg";
-
-const YouTubeEmbedAuto = () => {
-  // Build embed URL with autoplay, muted and loop (playlist=VIDEO_ID required for loop)
-  const src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&mute=1&loop=1&playlist=${VIDEO_ID}&rel=0`;
+const YouTubeEmbed = () => {
+  const [play, setPlay] = useState(false);
 
   return (
     <div className="flex justify-center items-center py-6">
       <div className="w-full max-w-3xl aspect-video rounded-2xl overflow-hidden shadow-lg relative">
-        <iframe
-          className="w-full h-full"
-          src={src}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        />
+        {!play ? (
+          <div
+            className="w-full h-full cursor-pointer relative"
+            onClick={() => setPlay(true)}
+          >P
+            {/* Preview Image */}
+            <img
+              src="https://res.cloudinary.com/de4ks8mkh/image/upload/v1758607984/WhatsApp_Image_2025-09-23_at_01.04.00_a9a55e7c_v8iczu.jpg"
+              alt="Video Preview"
+              className="w-full h-96 object-cover"
+            />
+            {/* Play Button Overlay */}
+            <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-40">
+              <div className="bg-white text-red-600 rounded-full p-4 shadow-lg">
+                ▶
+              </div>
+            </div>
+          </div>
+        ) : (
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/Qx6n3mgY4tg?si=iwXGTkeiSs76ANG3"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        )}
       </div>
     </div>
   );
 };
 
-export default YouTubeEmbedAuto;
+export default YouTubeEmbed;
