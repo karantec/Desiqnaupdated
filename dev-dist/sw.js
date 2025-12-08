9231dd7925a27e7fe5b5e2d697c035d00c071cbc
karantec <karanrana3095@gmail.com>
2025-12-09 01:38:56 +0530

    finalcommit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b7eb4f84'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qshdejr3h38"
+    "revision": "0.an3gva90pn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
