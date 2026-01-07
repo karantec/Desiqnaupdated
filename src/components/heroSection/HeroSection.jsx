82a7b06aec5826e16a8b178af9cb1a6ff76f189b
KARAN RANA <103360966+karantec@users.noreply.github.com>
2026-01-07 06:44:42 +0000

    finalcommit

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -145,7 +145,7 @@ function HeroSection() {
                 <li className="mb-6  md:items-start  leading-normal  text-md text-gray-900 font-sans font-bold">
                   <div className="flex items-start">
                     <FaArrowAltCircleRight className="text-orange-500 mt-1 mr-2 flex-shrink-0" />
-                    Price from Jan 1 - {"    "}
+                    Price from Jan 6 - {"    "}
             
                     <span className="text-2xl ml-4">₹10000</span>
                     {"    "} {"    "}
