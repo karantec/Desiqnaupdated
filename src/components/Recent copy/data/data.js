c340561bd06591a7063c4266ee767d41138d0e59
karantec <karanrana3095@gmail.com>
2026-01-31 19:23:51 +0530

    commit

--- src/components/Recent copy/data/data.js
+++ src/components/Recent copy/data/data.js
@@ -11,50 +11,63 @@ export const data = {
 
   user1: {
     id: 1,
+    p: "Coding Begineer to OffCampus SDE- Internship Offer from Ripling",
+    h1: "Kumar K Mentoring Student cracked Off-Campus SDE- Internship Offer from Ripling after 8 months of advanced DSA(OA + CP + System Design) training.",
+    company:
+      "https://assets.techrepublic.com/uploads/2024/02/tr_20240215-rippling-launch-australia.jpg",
+    student: "Watch Post",
+    video:
+      "https://www.linkedin.com/posts/kumark1_dsa-rippling-activity-7414314567738236929-Eh4P/",
+    img: "https://res.cloudinary.com/derywmjpv/image/upload/v1769867409/WhatsApp_Image_2026-01-31_at_7.18.34_PM_dewq8m.jpg",
+  },
+
+  user2: {
+    id: 2,
     p: "Student cracks 51 Lakhs/Year SDE-offer from Microsoft.",
     h1: "Kumar K Mentoring Student cracked Off-Campus Full-Time SDE Offer from Microsoft after 8 months of advanced DSA(OA + CP + System Design) training.",
     company:
       "https://static.vecteezy.com/system/resources/previews/018/911/396/non_2x/microsoft-logo-editorial-free-vector.jpg",
     student: "Watch Video",
-    video: "https://www.youtube.com/embed/n8xrsch5Ims?si=jAjbZak4x1_w8lUV",
+    video: "https://www.youtube.com/embed/n8xrsch5Ims",
     img: "https://res.cloudinary.com/derywmjpv/image/upload/v1768379052/n8xrsch5Ims-HD_ea7pa6.jpg",
   },
-  user2: {
-    id: 2,
+
+  user3: {
+    id: 3,
     p: "Tier-3 College to Google + Amazon Internship",
     h1: "Kumar K Mentoring Student cracked SDE- Internship Offer from Google and Amazon after 8 months of advanced DSA(OA + CP + System Design) training.",
     company:
-      "https://imgs.search.brave.com/we4J9Nce1CapScBva4Ygw_EXcYv5Jcson02x0CePSjs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE2LzEw/L0FtYXpvbi1Mb2dv/LTIwMDAtNTAweDI4/MS5wbmc",
+      "https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo-2000x500.png",
     student: "Watch Video",
     video: "https://www.youtube.com/shorts/imTzPWC9CPo",
     img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514904/amazon2_d0mack.png",
   },
 
-  user3: {
-    id: 3,
+  user4: {
+    id: 4,
     p: "Tier-3 College to OffCampus SDE- Internship Offer from Adobe",
-    img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514902/Anya_kuiyzi.png",
     h1: "Kumar K student cracks OffCampus Adobe SDE internship offer after 8 months of high level DSA + OA + CP training and 1-1 Mentorship",
+    company:
+      "https://logos-world.net/wp-content/uploads/2020/06/Adobe-Logo.png",
     student: "Anya Sharma (View Post)",
     video:
       "https://www.linkedin.com/posts/kumark1_ramram-kumark-student-activity-7367179005877313538-Ch7z/",
-    company:
-      "https://imgs.search.brave.com/nD3yKjOY9c2xNAOXoGzR2sFxNjutwgRAjaqLjhWa1pA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/cy13b3JsZC5uZXQv/d3AtY29udGVudC91/cGxvYWRzLzIwMjAv/MDYvQWRvYmUtTG9n/by0xOTkzLTIwMTQt/NzAweDM5NC5wbmc",
-  },
-
-  user4: {
-    id: 4,
-    p: "Tier-3 College to OffCampus SDE- Internship Offer from Amazon",
-    img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514903/vyom1_q3jkxz.png",
-    h1: "Kumar K student cracks OffCampus Amazon SDE internship offer after 8 months of high level DSA + OA + CP training and 1-1 Mentorship",
-    student: "Vyom Goyal (View Post)",
-    video:
-      "https://www.linkedin.com/posts/vyom-goyal-09ab5b246_learningjourney-dsa-codingjourney-activity-7387405007480025088-vthk",
-    company: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png",
+    img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514902/Anya_kuiyzi.png",
   },
 
   user5: {
     id: 5,
+    p: "Tier-3 College to OffCampus SDE- Internship Offer from Amazon",
+    h1: "Kumar K student cracks OffCampus Amazon SDE internship offer after 8 months of high level DSA + OA + CP training and 1-1 Mentorship",
+    company: "https://1000logos.net/wp-content/uploads/2016/10/Amazon-Logo.png",
+    student: "Vyom Goyal (View Post)",
+    video:
+      "https://www.linkedin.com/posts/vyom-goyal-09ab5b246_learningjourney-dsa-codingjourney-activity-7387405007480025088-vthk",
+    img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514903/vyom1_q3jkxz.png",
+  },
+
+  user6: {
+    id: 6,
     p: "Student cracks 51 Lakhs/Year SDE-offer from Amazon.",
     h1: "Kumar K Mentoring Student cracked Off-Campus Full-Time SDE Offer from Amazon after 8 months of advanced DSA(OA + CP + System Design) training.",
     company: "https://tse2.mm.bing.net/th/id/OIP.ynG9bDG-UKi5-HizOP8UeQHaHa",
@@ -63,31 +76,30 @@ export const data = {
     img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767513188/maxresdefault_2_d4y3qs.jpg",
   },
 
-  user6: {
-    id: 6,
+  user7: {
+    id: 7,
     p: "Student cracks 27 Lakhs/Year SDE-offer from Qualcomm",
     h1: "Kumar K Mentoring Student cracked Off-Campus Full-Time SDE offer from Qualcomm after 8 months of advanced DSA(OA + CP + System Design) training.",
-    company:
-      "https://imgs.search.brave.com/4SViid5zuUKSMjuT1TngOwvNZ-P8tVVX_x4AevN6hIY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy82/MmJiMWZiNmI2MTcz/ZDUxZGZjNDc0YjIu/cG5n",
+    company: "https://assets.stickpng.com/images/62bb1fb6b6173d51dfc474b2.png",
     student: "Watch Video",
     video: "https://www.youtube.com/watch?v=q9yByJgWV6M",
     img: "https://img.youtube.com/vi/q9yByJgWV6M/maxresdefault.jpg",
   },
 
-  user7: {
-    id: 7,
+  user8: {
+    id: 8,
     p: "Student cracks 30 Lakhs/Year SDE-offer from Commvault",
     h1: "Kumar K Mentoring Student cracked Off-Campus Full-Time SDE offer from Commvault after 8 months of advanced DSA(OA + CP + System Design) training.",
     company:
-      "https://imgs.search.brave.com/wYcxhhIBOX7VMDl_JYaJTpHI2GW_yZb6gVSEpElgD0c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZzJjcm93ZC5j/b20vdXBsb2Fkcy9w/cm9kdWN0L2ltYWdl/LzczZTkyYWQzOTA1/NDNkOGQyOTIwZGEx/YjNhYzc3OTRkL2Nv/bW12YXVsdC1jbG91/ZC5wbmc",
+      "https://images.g2crowd.com/uploads/product/image/large_detail/large_detail_73e92ad390543d8d2920da1b3ac7794d/commvault-cloud.png",
     student: "Revanth Srinivasa Reddy (Watch Post)",
     video:
       "https://www.linkedin.com/posts/kumark1_commvault-coding-activity-7401655430009282561-32Z4",
     img: "https://res.cloudinary.com/derywmjpv/image/upload/v1767514903/Revanth1_pw6ph9.png",
   },
 
-  user8: {
-    id: 8,
+  user9: {
+    id: 9,
     p: "Kumar K Student cracks 40LPA SDE-offer from PayPal",
     h1: "Kumar K Mentoring Student cracked Off-Campus Full-Time SDE Offer from PayPal after 8 months of advanced DSA(OA + CP + System Design) training.",
     company:
@@ -97,8 +109,8 @@ export const data = {
     img: "kishorMpaypl.jpg",
   },
 
-  user9: {
-    id: 9,
+  user10: {
+    id: 10,
     p: "Tier-4 College to OffCampus SDE-Offer from BrowserStack",
     h1: "Kumar K Mentoring Student cracks OffCampus SDE-Offer from BrowserStack after 8 months of advanced DSA(OA + CP + System Design) training + OffCampus Referral Support",
     company:
@@ -108,18 +120,18 @@ export const data = {
     img: "akash1.jpg",
   },
 
-  user10: {
-    id: 10,
+  user11: {
+    id: 11,
     p: "Kumar K Student cracks OffCampus SDE-offer from Myntra!",
     h1: "Mentoring Student cracks OffCampus SDE-offer from Myntra after 8 months of advanced DSA(OA + CP + System Design) training.",
     company:
-      "https://imgs.search.brave.com/fyXkkCKsIG1VoMBhlBVAkchWzQ97-X_M0lrxf80goOw/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9leHRl/cm5hbC1wcmV2aWV3/LnJlZGQuaXQvUXFC/TTYzWk5tRWlSZWxq/em1TYWtuSld0SjM2/WG9rbG15TGprWW1r/bVJpWS5qcGc_d2lk/dGg9NjQwJmNyb3A9/c21hcnQmYXV0bz13/ZWJwJnM9N2VjMzcy/YmU5NjIxZmQ0M2U4/ZmRmYTcyZDFhYjQ5/Mjg0NzljYmYxMg",
+      "https://logos-world.net/wp-content/uploads/2020/04/Myntra-Logo.png",
     student: "Kumar Harsh",
     img: "harsh.jpg",
   },
 
-  user11: {
-    id: 11,
+  user12: {
+    id: 12,
     p: "From Waiter to 40LPA SDE-2 at Europe Based MNC",
     h1: "Mentoring Student cracks SDE-2 offer from Europe Based MNC BCG-X after 8 months of advanced DSA(OA + CP + System Design) training",
     company:
@@ -129,8 +141,8 @@ export const data = {
     img: "anad.png",
   },
 
-  user12: {
-    id: 12,
+  user13: {
+    id: 13,
     p: "Kumar K Student cracks OffCampus offer from Amazon",
     h1: "Kumar K Mentoring Student cracks OffCampus offer from Amazon",
     company:
@@ -141,8 +153,8 @@ export const data = {
     img: "abiram.jpg",
   },
 
-  user13: {
-    id: 13,
+  user14: {
+    id: 14,
     p: "Kumar K Student cracks offer from Google!",
     h1: "Kumar K Mentoring Student cracks offer from Google after 8 months of advanced DSA(OA + CP) training",
     company:
@@ -151,35 +163,25 @@ export const data = {
     img: "shivamg.jpg",
   },
 
-  user14: {
-    id: 14,
+  user15: {
+    id: 15,
     p: "Kumar K Student cracks offer from Directi and Zomato",
     h1: "Mentoring Student cracks offer from Directi and Zomato after 8 months of advanced DSA(OA + CP + System Design) training",
     company:
-      "https://imgs.search.brave.com/iIfIU6Q2wjRtp6GxvLRySomBiiHR77-AX3QsO8q6Rlw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/ZG93bmxvYWQub3Jn/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIw/LzAyL3pvbWF0by1s/b2dvLnBuZw",
+      "https://logodownload.org/wp-content/uploads/2020/02/zomato-logo.png",
     student: "Aaditya Gupta (View Post)",
     video:
       "https://www.linkedin.com/posts/kumark1_ramram-kumark-coding-activity-7360692834254938113-ZD6E",
     img: "adit.jpg",
   },
 
-  user15: {
-    id: 15,
+  user16: {
+    id: 16,
     p: "World Rank 160/30000 participants in LeetCode contest",
     h1: "Kumar K Mentoring Student Achieves World Rank 160/30000 in LeetCode contest",
     company:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5bzCI3h8HZdh83wkNL8FY535Zzfum_v2DiA",
     student: "Aman Singh",
     img: "amans.jpg",
-  },
-
-  user16: {
-    id: 16,
-    p: "Kumar K student Achieved 1800+ Rating on Leetcode",
-    h1: "Mentoring Student Achieved 1800+ Rating on Leetcode and got Knight badge after 8 months of advanced DSA(OA + CP) training",
-    company:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5bzCI3h8HZdh83wkNL8FY535Zzfum_v2DiA",
-    student: "Asmit Shukla",
-    img: "asmit.jpg",
   },
 };
